61361062404646a1d2ef56c4035254426e05bf5b
osh <dhtpgus7@gmail.com>
2022-04-11 23:17:23 +0900

    타입스크립트 맛보기

---
+++ typescript-styled-components/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Circle from "./Circle";
+
+function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
+      {/* <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="send text" bgColor="tomato" /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ typescript-styled-components/src/Circle.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${({ bgColor }) => bgColor};
+  border-radius: 100px;
+  border: 4px solid ${(props) => props.borderColor};
+`;
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;

1663646f21f79c45612904e0171cf97cb81fbef3
osh <dhtpgus7@gmail.com>
2022-04-14 21:57:59 +0900

    타입 스타일컴포넌트 디클레어 추가 방법

--- typescript-styled-components/src/App.tsx
+++ typescript-styled-components/src/App.tsx
@@ -1,34 +1,20 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Circle from "./Circle";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-      {/* <Circle borderColor="yellow" bgColor="teal" />
-      <Circle text="send text" bgColor="tomato" /> */}
-    </div>
+    <Container>
+      <H1>pro</H1>
+    </Container>
   );
 }
 

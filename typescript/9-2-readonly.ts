abefaa900a1099e0752fce3646df3b5fa6fb50ed
OSH <42954693+oshosh@users.noreply.github.com>
2022-07-10 20:18:28 +0900

    Create 9-2-readonly.ts

---
+++ typescript/9-2-readonly.ts
@@ -0,0 +1,11 @@
+{
+  type ToDo = {
+    title: string;
+  };
+
+  const todo: Readonly<ToDo> = {
+    title: "Delete inactive users",
+  };
+
+  todo.title = "Hello"; // 읽기 전용 속성이므로 'title'에 할당할 수 없습니다.
+}
